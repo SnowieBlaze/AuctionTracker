84bdc15a921d3d9e8313faaf4504bd053c54726a
Alexandru Theodor Tarcuta <a.t.tarcuta@student.tudelft.nl>
2025-03-12 17:39:00 +0100

    Initial setup

--- server/services/auction_data_grabber.py
+++ server/services/auction_data_grabber.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-def get_auction_data():
-    url = "https://api.hypixel.net/v2/skyblock/auctions"
+def get_auction_data(url):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
